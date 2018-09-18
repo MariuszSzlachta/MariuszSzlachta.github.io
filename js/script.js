e642cc9557ceb09dfb0e7715c568b42542fe0685
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-09-18 12:58:50 +0200

    Add carousel

--- js/script.js
+++ js/script.js
@@ -14,4 +14,20 @@ const flkty = new Flickity(element, {
     contain: true,
     hash: true,
     pageDots: true
+});
+
+// Projects controll
+
+// Project-1
+
+const showDetailsButton1 = document.querySelector('[data-get-details="1"]');
+const details1 = document.querySelector('[data-details="1"]');
+const preview1 = document.querySelector('[data-preview="1"]');
+const headings1 = document.querySelector('[data-headings="1"]');
+
+showDetailsButton1.addEventListener('click', function(e){
+    e.preventDefault();
+    headings1.classList.remove('headings--active');
+    preview1.classList.remove('preview--active');
+    details1.classList.add('details--active');
 })
